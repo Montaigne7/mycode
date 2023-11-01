777aa243a211cc4d2b80f95fe8b7d945b6a36b36
Montaigne7 <zaratehimber01@gmail.com>
2023-11-01 21:29:35 +0000

    Module 13 challenge program

---
+++ challenge_13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#ask user's name for input to name variable
+name = input("What is your name? ")
+
+#ask for the day of the week for name variable
+day = input("What is the day of the week? ")
+
+#display message with user's name and day of the week
+print("Hello,",  name + "! Happy", day + "!")
