6b7ff37e48f5139eebeb664e577013779072c0e8
Montaigne7 <zaratehimber01@gmail.com>
2023-11-06 21:07:24 +0000

    revision of iptaker program

--- lab_input/iptaker01.py
+++ lab_input/iptaker01.py
@@ -6,10 +6,16 @@
 def main():
 
     # pause the program and wait for the user to provide input
-    user_input = input("Please enter an IPv4 IP address:")
+    user_input = input("Please enter an IPv4 IP address: ")
     
     # display the input back to the user.
     print("You told me the IPv4 address is: " + user_input)
+    
+    #pause pogram and take more of user's input
+    user_input = input("Please enter the default gateway: ")
+
+    #display input back to the user.
+    print("You told me the default gateway is:", user_input)
     
 # this calls main
 main()
