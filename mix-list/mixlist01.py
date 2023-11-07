aee373175186bea9cb11969237c9f95876e58a1e
Montaigne7 <zaratehimber01@gmail.com>
2023-11-07 05:33:55 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Create a list containing three items
+my_list = ["192.168.0.5", 5060, "UP"]
+
+print("The first item in the list (IP): " + my_list[0])
+# will display first item on list
+
+print("The second item in the list (port): " + str(my_list[1]))
+# will display second item on list
+
+print("The last item in the list (state): " + my_list[2])
+# will display last item on list
+
+# create a list witht he following items
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print("The following IP Addresses are:\n" + iplist[3] + "\n" + iplist[4])
